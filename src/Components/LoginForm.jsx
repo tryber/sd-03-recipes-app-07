5272352b737eea35f861011f6a8ecd0fb2d7805d
luldelrey <rpgstrikered@gmail.com>
2020-06-25 16:58:28 -0300

    login form renderiza componentes e valida email e senha

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const LoginForm = () => (
-  <div>
-    <label htmlFor="email">
-      Email:
-      <input
-        id="email"
-        type="email"
-        placeholder="Digite seu Email"
-      />
-    </label>
-    <label htmlFor="password">
-      Senha:
-      <input
-        id="password"
-        type="password"
-        placeholder="Digite sua Senha"
-      />
-    </label>
-  </div>
+const RenderEmail = (callback, value) => (
+  <label htmlFor="email">
+    Email:
+    <input
+      data-testid="email-input"
+      id="email"
+      placeholder="Digite seu Email"
+      type="email"
+      onChange={(e) => callback(e.target.value)}
+      value={value}
+    />
+  </label>
 );
+
+const RenderPassword = (callback, value) => (
+  <label htmlFor="password">
+    Senha:
+    <input
+      data-testid="password-input"
+      id="password"
+      placeholder="Digite sua Senha"
+      type="password"
+      onChange={(e) => callback(e.target.value)}
+      value={value}
+    />
+  </label>
+);
+
+const RenderLoginButton = (disabled) => (
+  <button
+    data-testid="login-submit-btn"
+    type="button"
+    disabled={disabled}
+  >
+    Entrar
+  </button>
+);
+
+const LoginForm = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const disableButton = () => {
+      const regex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[A-Za-z]+$/.test(email);
+      if (regex && password.length >= 6) {
+        return setDisabled(false);
+      }
+      return setDisabled(true);
+    };
+
+    disableButton();
+  }, [email, password]);
+
+  return (
+    <div>
+      {RenderEmail(setEmail, email)}
+      <br />
+      {RenderPassword(setPassword, password)}
+      <br />
+      {RenderLoginButton(disabled)}
+    </div>
+  );
+};
 
 export default LoginForm;
