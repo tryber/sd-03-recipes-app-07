e8f64e3e6f7d291baa13d52345848e583619f4c7
luldelrey <rpgstrikered@gmail.com>
2020-06-29 17:07:46 -0300

    pagina de detalhes da receita criada

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const DetailsPage = () => (
-  <div>
-    <h1>Página de detalhes</h1>
-    <p>Detalhes da receita: </p>
-  </div>
-);
+const DetailsPage = (props) => {
+  const { location: { state } } = props;
+  if (state) {
+    console.log('tem estado');
+    return (
+      <div>
+        <h1>Página de detalhes</h1>
+        {console.log('history:', state)}
+      </div>
+    );
+  }
+  if (!state) {
+    console.log('não tem estado, fazer requisiçao');
+  }
+  return <h1>Loading...</h1>;
+};
+
+DetailsPage.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.object,
+  }).isRequired,
+};
 
 export default DetailsPage;
