719bf497470a3064a1f5562f2ff563243fae964d
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-06-29 19:17:11 -0300

    Header 90% finalizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,66 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { recipeContext } from '../Hooks/recipeContext';
 import profile from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-const SearchButton = () => (
-    <button data-testid="search-top-btn">
-      <img src={searchIcon} alt="botao-de-busca" />
-    </button>
-  );
+const SearchButton = (callBack, value) => (
+  <button
+    data-testid="search-top-btn"
+    onClick={() => callBack(value)}
+  >
+    <img src={searchIcon} alt="botao-de-busca" />
+  </button>
+);
+
+const radioButtons = () => (
+  <div className="radios-box">
+    <input
+      value="ingredients"
+      type="radio"
+      name="selectButtonsradios"
+      data-testid="ingredient-search-radio"
+      onChange={(event) => console.log(event.target.value)}
+    />
+    <label htmlFor="selectButtonsradios">Ingredientes</label>
+    <input
+      value="name"
+      type="radio"
+      name="selectButtonsradios"
+      data-testid="name-search-radio"
+      onChange={(event) => console.log(event.target.value)}
+    />
+    <label htmlFor="selectButtonsradios">Nome</label>
+    <input
+      value="letterFirst"
+      type="radio"
+      name="selectButtonsradios"
+      data-testid="first-letter-search-radio"
+      onChange={(event) => console.log(event.target.value)}
+    />
+    <label htmlFor="selectButtonsradios">Primeira letra</label>
+  </div>
+);
+
+const SearchBar = () => (
+  <div>
+    <label htmlFor="searchInput">
+      <input
+        name="searchInput"
+        type="text"
+        data-testid="search-input"
+        placeholder="Buscar receitas"
+      />
+    </label>
+    {radioButtons()}
+  </div>
+);
 
 const Header = () => {
 
-  const { titlePage } = useContext(recipeContext);
+  const { titlePage, foods } = useContext(recipeContext);
+  console.log('fernando:',foods)
+  const [showSearchBar, setShowSearchBar] = useState(false);
 
   return (
     <nav>
@@ -23,7 +71,9 @@ const Header = () => {
         />
       </Link>
       <h1 data-testid="page-title">{titlePage}</h1>
-      {SearchButton()}
+      {SearchButton(setShowSearchBar, !showSearchBar)}
+      {showSearchBar && SearchBar()}
+      
     </nav>
   );
 };
