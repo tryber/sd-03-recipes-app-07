1f381a372f954151c3bdd6293cb682ee1015d5c5
luldelrey <rpgstrikered@gmail.com>
2020-06-30 21:04:48 -0300

    cc

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -164,12 +164,13 @@ const makeTheDish = (dish, recomendations) => {
   ));
 };
 
+const mealOrDrink = (meal, drink) => { if (meal) return meal[0]; return drink[0]; };
+
+const goodRecomen = (value) => { if (value.meals) return value.meals; return value.drinks; };
+
 const DetailsPage = () => {
   const { params: { id }, path } = useRouteMatch();
   const { recipe, recomendations, requesting } = useRequest(path, id);
-
-  const mealOrDrink = (meal, drink) => { if (meal) return meal[0]; return drink[0]; };
-  const goodRecomen = (value) => { if (value.meals) return value.meals; return value.drinks; };
 
   if (!requesting && !recipe.meals && !recipe.drinks) return <h1>Receita não encontrada</h1>;
   if (!requesting && recipe) {
