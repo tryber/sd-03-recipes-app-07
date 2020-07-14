afb999f8d347df2ff2646191b10898fc34202ac2
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-07-14 14:30:43 -0300

    Em busca dos 100%

--- src/Components/inProgressPage/RenderInProgressDish.jsx
+++ src/Components/inProgressPage/RenderInProgressDish.jsx
@@ -13,12 +13,12 @@ const renderTitles = (title, category) => (
 const renderHeading = (path, favorites, setFavorite, callback, state, {
   id, type, area, category, drinkCategory, alcoholicOrNot, title, thumb,
 }) => (
-    <div className="recipe-header">
-      {renderTitles(title, category)}
-      {renderButtons(path, favorites, setFavorite, callback, state, {
-        id, type, area, category, drinkCategory, alcoholicOrNot, title, thumb,
-      })}
-    </div>
+  <div className="recipe-header">
+    {renderTitles(title, category)}
+    {renderButtons(path, favorites, setFavorite, callback, state, {
+      id, type, area, category, drinkCategory, alcoholicOrNot, title, thumb,
+    })}
+  </div>
   );
 
 const checkIfIncludes = (id, i, checks, type) => {
@@ -98,22 +98,22 @@ const RenderDish = (callback, state, {
   id, type, area, drinkCategory, alcoholicOrNot = '', path, favorites, thumb, title,
   category, ingredients, measures, instructions, setFavorite, checks, func,
 }) => (
-    <div style={{paddingBottom: '58px'}}>
-      <img
-        alt="food or beverage"
-        className="recipe-img"
-        data-testid="recipe-photo"
-        src={thumb}
-      />
-      <div className="recipe-container">
-        {renderHeading(path, favorites, setFavorite, callback, state, {
-          id, type, area, category, drinkCategory, alcoholicOrNot, title, thumb,
-        })}
-        {renderIngredients(id, type, ingredients, measures, checks, func[0], func[1])}
-        {renderIntructions(instructions)}
-      </div>
-      {startBtn(checkIfAllMarket(ingredients, id, type, checks))}
+  <div style={ { paddingBottom: '58px' } }>
+    <img
+      alt="food or beverage"
+      className="recipe-img"
+      data-testid="recipe-photo"
+      src={thumb}
+    />
+    <div className="recipe-container">
+      {renderHeading(path, favorites, setFavorite, callback, state, {
+        id, type, area, category, drinkCategory, alcoholicOrNot, title, thumb,
+      })}
+      {renderIngredients(id, type, ingredients, measures, checks, func[0], func[1])}
+      {renderIntructions(instructions)}
     </div>
+    {startBtn(checkIfAllMarket(ingredients, id, type, checks))}
+  </div>
   );
 
 export default RenderDish;
